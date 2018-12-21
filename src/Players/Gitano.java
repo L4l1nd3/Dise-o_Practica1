dcec40aca7ba7964da8f0aaa7215d6547634ab9f
Sean - portatil <seannjust@gmail.com>
2018-12-21 18:33:25 +0100

    Revert "Personajes Vallecas"
    This reverts commit 12ad6b5512ad1a24e74c35e06db95e722cfea59e.

--- src/Players/Gitano.java
+++ src/Players/Gitano.java
@@ -2,7 +2,6 @@ package Players;
 
 public abstract class Gitano extends Enemy {
 
-
 	Gitano(int intelecto, int fuerza, int HP, int mana, String nombre) {
 		super(intelecto, fuerza, HP, mana, nombre);
 		// TODO Auto-generated constructor stub
--- src/Players/V_Vallecas.java
+++
@@ -1,9 +0,0 @@
-package Players;
-
-public class V_Vallecas {
-
-	public V_Vallecas() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
